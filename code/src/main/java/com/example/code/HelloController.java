b974b6aca38ce746527e5f0ec994270c0eb9a373
User <user@debian-BULLSEYE-live-builder-AMD64>
2022-10-31 17:35:13 +0100

    Login avec email

--- code/src/main/java/com/example/code/HelloController.java
+++ code/src/main/java/com/example/code/HelloController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,8 +18,13 @@ public class HelloController {
     private Scene scene;
     private Parent root;
     @FXML
+    private TextField emailField;
+    @FXML
+    private String email;
+    @FXML
     public void connecter(ActionEvent event) throws IOException {
-        System.out.println("Allez yes");
+        email = emailField.getText();
+        System.out.println(email);
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("appli.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
