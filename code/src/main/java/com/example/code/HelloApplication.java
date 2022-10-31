b48bf70110cc589f7acf4c3882a9a31e3f8b1518
User <user@debian-BULLSEYE-live-builder-AMD64>
2022-10-31 18:38:33 +0100

    Icone

--- code/src/main/java/com/example/code/HelloApplication.java
+++ code/src/main/java/com/example/code/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.code;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
@@ -11,6 +12,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Image image = new Image(getClass().getClassLoader().getResource("icon.jpg").toExternalForm());
+        stage.getIcons().add(image);
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Biblio");
