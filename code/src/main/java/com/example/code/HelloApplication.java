43ed954d63250cec564df68d6a440533508c22bf
User <user@debian-BULLSEYE-live-builder-AMD64>
2022-10-31 14:59:30 +0100

    Imports

--- code/src/main/java/com/example/code/HelloApplication.java
+++ code/src/main/java/com/example/code/HelloApplication.java
@@ -1,17 +1,18 @@
 package com.example.code;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Group root = new Group();
+        Scene scene = new Scene();
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
