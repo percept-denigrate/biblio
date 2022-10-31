e4170da1a73aa7019c52955daf344e8335888303
User <user@debian-BULLSEYE-live-builder-AMD64>
2022-10-31 16:27:05 +0100

    Test login

--- code/src/main/java/com/example/code/HelloController.java
+++ code/src/main/java/com/example/code/HelloController.java
@@ -1,8 +1,13 @@
 package com.example.code;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
 
+    @FXML
+    void connecter(ActionEvent event) {
+
+    }
 }
