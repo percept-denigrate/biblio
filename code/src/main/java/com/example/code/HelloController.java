4a3d2a4a3f1c71ae1c54fbbc3ff7c81cf4fd58ab
User <user@debian-BULLSEYE-live-builder-AMD64>
2022-11-02 17:35:54 +0100

    Factorisation en une requete

--- code/src/main/java/com/example/code/HelloController.java
+++ code/src/main/java/com/example/code/HelloController.java
@@ -312,22 +312,13 @@ public class HelloController {
             String sql =  //livres empruntes et rendus
                     "SELECT Auteur.prenom,Auteur.nom,Oeuvre.titre,Oeuvre.date,Edition.ISBN,Edition.editeur,Edition.annee " +
                     "FROM Auteur JOIN Ecriture JOIN Oeuvre JOIN Edition JOIN " +
-                        "(SELECT Livre.id,ISBN FROM Livre JOIN Emprunt ON Livre.id=Emprunt.livre GROUP BY Livre.id HAVING COUNT(*)=COUNT(Emprunt.fin)) AS LivreE " +
-                    "ON Auteur.id=Ecriture.Auteur AND Ecriture.oeuvre=Oeuvre.id AND Oeuvre.id=Edition.oeuvre AND Edition.ISBN=LivreE.ISBN;";
+                        "((SELECT Livre.id,ISBN FROM Livre JOIN Emprunt ON Livre.id=Emprunt.livre GROUP BY Livre.id HAVING COUNT(*)=COUNT(Emprunt.fin)) UNION " +
+                            "(SELECT * FROM Livre WHERE Livre.id NOT IN (SELECT Emprunt.livre FROM Emprunt))) AS Livre " +
+                            "ON Auteur.id=Ecriture.Auteur AND Ecriture.oeuvre=Oeuvre.id AND Oeuvre.id=Edition.oeuvre AND Edition.ISBN=Livre.ISBN;";
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
                 inventaireD.getItems().add(new Livre(rs.getString("titre"), rs.getString("prenom") + " " + rs.getString("nom"), rs.getInt("date"), rs.getString("editeur") + " " + rs.getInt("annee"), rs.getLong("ISBN")));
-            }
-            sql =  //livres jamais empruntes
-                    "SELECT Auteur.prenom,Auteur.nom,Oeuvre.titre,Oeuvre.date,Edition.ISBN,Edition.editeur,Edition.annee " +
-                    "FROM Auteur JOIN Ecriture JOIN Oeuvre JOIN Edition JOIN " +
-                        "(SELECT * FROM Livre WHERE Livre.id NOT IN (SELECT Emprunt.livre FROM Emprunt)) AS Livre " +
-                    "ON Auteur.id=Ecriture.Auteur AND Ecriture.oeuvre=Oeuvre.id AND Oeuvre.id=Edition.oeuvre AND Edition.ISBN=Livre.ISBN;";
-            Statement stmt2 = con.createStatement();
-            ResultSet rs2 = stmt2.executeQuery(sql);
-            while (rs2.next()) {
-                inventaireD.getItems().add(new Livre(rs2.getString("titre"), rs2.getString("prenom") + " " + rs2.getString("nom"), rs2.getInt("date"), rs2.getString("editeur") + " " + rs2.getInt("annee"), rs2.getLong("ISBN")));
             }
             con.close();
         }catch(Exception e){ System.err.println(e);}
